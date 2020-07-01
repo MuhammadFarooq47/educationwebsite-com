afa23c8440aa6e39f6925780f07232d072cc9b1d
MuhammadFarooq47 <64726118+MuhammadFarooq47@users.noreply.github.com>
2020-07-01 22:41:09 +0500

    Add files via upload
    Educational Website...

---
+++ app.js
@@ -0,0 +1,35 @@
+function openNav() {
+    var open = document.querySelector("nav");
+    open.style.left= "0%";
+}
+
+function closeNav() {
+    var open = document.querySelector("nav");
+    open.style.left = "-1000%";
+}
+
+function myFunction(){
+    var x=document.getElementById("input");
+    if (x.type==="password")
+    {x.type="text"}
+     
+     else{x.type="password"}
+}  
+
+function dear(){
+    alert ("Please wait while you are redirected your email.")
+}
+        
+   
+     
+     
+     
+     
+     
+     
+    
+    
+    
+    
+
+
